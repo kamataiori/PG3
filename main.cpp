d74166f12d94afcc6d9da9c3c489ade405e44b09
LE2B_16 <k023g1064@g.neec.ac.jp>
2025-02-07 21:01:37 +0900

    完了

--- main.cpp
+++ main.cpp
@@ -1,10 +1,33 @@
 #include <stdio.h>
+#include <iostream>
+#include <string>
+#include <chrono>
 
-int main() {
+    int main() {
+        // 100,000文字の 'a' で初期化
+        std::string a(100000, 'a');
 
-	printf("Hello World\n");
-	printf("こんにちわ\n");
-	printf("PG3");
+        // コピー計測開始
+        auto start_copy = std::chrono::high_resolution_clock::now();
+        std::string b = a;  // コピー
+        auto end_copy = std::chrono::high_resolution_clock::now();
 
-	return 0;
-}
+        // 移動計測開始
+        auto start_move = std::chrono::high_resolution_clock::now();
+        std::string c = std::move(a);  // 移動
+        auto end_move = std::chrono::high_resolution_clock::now();
+
+        // 計測結果をμs(マイクロ秒)単位で取得
+        auto copy_time = std::chrono::duration_cast<std::chrono::microseconds>(end_copy - start_copy).count();
+        auto move_time = std::chrono::duration_cast<std::chrono::microseconds>(end_move - start_move).count();
+
+        // printf で出力
+        printf("100,000文字を移動とコピーで比較しました。\n");
+        printf("コピー : %lldμs\n", copy_time);
+        printf("移動   : %lldμs\n", move_time);
+        printf("続行するには何かキーを押してください ．．．");
+
+        getchar();  // キー入力待機
+
+        return 0;
+    }
